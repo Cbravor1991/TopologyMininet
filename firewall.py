4f952c1a54a60e5e9c9bfab57bbc648745f0f584
luciapardo <lpardo@fi.uba.ar>
2022-06-29 17:15:58 -0300

    refactor firewall

--- firewall.py
+++ firewall.py
@@ -16,21 +16,23 @@ from pox.lib.addresses import EthAddr
 from collections import namedtuple
 import pox.lib.packet as pkt
 import os
-''' Add your imports here ... '''
 
 
 log = core.getLogger()
 policyFile = "%s/pox/pox/misc/firewall-policies.csv" % os.environ['HOME']
 
-''' Add your global variables here ... '''
 
+rules = (
 
-@staticmethod
-def log_icmp_msg(icmp_msg):
-    log.debug('Type: %d' % icmp_msg.type)
-    log.debug('Code: {:#x}'.format(icmp_msg.code))
-    log.debug('Checksum: {:#x}'.format(icmp_msg.csum))
-    log.debug('Payload: {}'.format(icmp_msg.payload))
+    {'dl_type': pkt.ethernet.IP_TYPE, 'tp_dst': 80,
+     'nw_proto': pkt.ipv4.TCP_PROTOCOL},
+
+    {'dl_type': pkt.ethernet.IP_TYPE, 'tp_dst': 80,
+     'nw_proto': pkt.ipv4.UDP_PROTOCOL},
+
+    {'dl_src': EthAddr('00:00:00:00:00:01'), 'dl_type': pkt.ethernet.IP_TYPE,
+     'tp_dst': 5001, 'nw_proto': pkt.ipv4.UDP_PROTOCOL},
+)
 
 
 class Firewall (EventMixin):
@@ -39,34 +41,11 @@ class Firewall (EventMixin):
         self.listenTo(core.openflow)
         log.debug("Enabling Firewall Module")
 
-    def filter_UDP_port_80(self, event):
-        match = of.ofp_match()
-        match.dl_type = pkt.ethernet.IP_TYPE
-        match.nw_proto = pkt.ipv4.UDP_PROTOCOL
-        match.tp_dst = 80
-        msg_port = of.ofp_flow_mod(match=match)
-        event.connection.send(msg_port)
-
-    def filter_TCP_port_80(self, event):
-        match = of.ofp_match()
-        match.dl_type = pkt.ethernet.IP_TYPE
-        match.nw_proto = pkt.ipv4.TCP_PROTOCOL
-        match.tp_dst = 80
-        msg_port = of.ofp_flow_mod(match=match)
-        event.connection.send(msg_port)
-
-    def filter_h1(self, event):
-        my_match = of.ofp_match()
-        my_match.dl_dst = EthAddr('00:00:00:00:00:01')
-        my_match.dl_type = pkt.ethernet.IP_TYPE
-        my_match.tp_dst = 5001
-        my_match.nw_proto = pkt.ipv4.UDP_PROTOCOL
-        msg = of.ofp_flow_mod(match=my_match)
-        event.connection.send(msg)
-
     def _handle_ConnectionUp(self, event):
-        self.filter_UDP_port_80(event)
-        self.filter_TCP_port_80(event)
+        for rule in rules:
+            match = of.ofp_match(**rule)
+            msg = of.ofp_flow_mod(match=match)
+            event.connection.send(msg)
         log.debug("Firewall rules installed on %s", dpidToStr(event.dpid))
 
 
